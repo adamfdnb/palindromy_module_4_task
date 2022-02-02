a3a7b1802e86c065c777d8c87ac9dbddcffe7be9
Adam Fieleborn <adamf.dnb@gmail.com>
2022-02-02 20:57:34 +0100

    Add function description

--- palindromy.py
+++ palindromy.py
@@ -1,4 +1,11 @@
 def is_palindrom (word):
+    """
+        Funkcja sprawdza czy słowo podane przez użytkownika to palindrom. 
+        Zwraca wartość True gdy palindrom
+        Zwraca wartość Fasle gdy nie palindrom 
+        Argument:
+        word
+    """
     print(f"{word[::-1] == word[::1]}")
 
 is_palindrom(input('wstaw słowo do sprawdzenia: '))
